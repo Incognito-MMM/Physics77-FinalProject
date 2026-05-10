33313ea05397652677151af6d829b4ccfec3b1f6
Incognito-MMM <sascha.isbjorn@berkeley.edu>
2026-05-10 16:11:30 -0700

    Update SpringPen(Length-Heatmap).py

--- V.C.1/SpringPen(Length-Heatmap).py
+++ V.C.1/SpringPen(Length-Heatmap).py
@@ -1,20 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.1
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # %%
 # !pip install numba
-
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
